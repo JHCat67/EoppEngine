e557298ee5d1835d103bbea20730abe98f993188
Jonathan Hollsten <jhcat67@gmail.com>
2025-12-30 06:34:33 +0100

    Added changes to the run scripts

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 rmdir /s /q build
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=C:/Users/JHCat/Downloads/vcpkg-master/vcpkg-master/scripts/buildsystems/vcpkg.cmake -A x64
+cmake .. -DCMAKE_TOOLCHAIN_FILE=vcpkg-master/vcpkg-master/scripts/buildsystems/vcpkg.cmake -A x64
 cmake --build .
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=C:/Users/JHCat/Downloads/vcpkg-master/vcpkg-master/scripts/buildsystems/vcpkg.cmake -A x64
+cmake .. -DCMAKE_TOOLCHAIN_FILE=vcpkg-master/vcpkg-master/scripts/buildsystems/vcpkg.cmake -A x64
 cmake --build .
