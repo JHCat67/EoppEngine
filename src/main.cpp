fb44048496fc0ce4ee48d07ae77896ac25c71a42
Jonathan Hollsten <jhcat67@gmail.com>
2025-12-30 02:31:50 +0100

    Made changes to main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+int main() {
+    return 0;
+}
