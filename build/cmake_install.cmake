43977bd1a0765f18db3aeaac63512c8523e14884
Jonathan Hollsten <jhcat67@gmail.com>
2025-12-30 05:19:33 +0100

    Changes to build

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/EoppEngine")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/EoppEngine")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
