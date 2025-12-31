380f10668c83e9da9043e166cc510392c68c259a
Jonathan Hollsten <jhcat67@gmail.com>
2025-12-31 01:43:46 +0100

    Fixed run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 rmdir /s /q build
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=vcpkg-master/vcpkg-master/scripts/buildsystems/vcpkg.cmake -A x64
+cmake .. -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -A x64
 cmake --build .
