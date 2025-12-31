7913172f3f201a18f50cc2ac8f9275f3f17f751b
Jonathan Hollsten <jhcat67@gmail.com>
2025-12-31 05:20:23 +0100

    Changes

--- src/engine/window/window.cpp
+++ src/engine/window/window.cpp
@@ -1,15 +1,15 @@
 #include "engine/window/window.hpp"
-#include "engine/window/screenResolution.hpp"
 
 Window::Window()
 {
-    ScreenResolution::getScreenResolution(width, height);
-
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         std::cout << "Error initializing SDL" << std::endl;
     }
 
-    window = SDL_CreateWindow("Eopp Engine", width, height, SDL_WINDOW_VULKAN);
+    SDL_DisplayID displayID = SDL_GetPrimaryDisplay();
+    const SDL_DisplayMode* mode = SDL_GetDesktopDisplayMode(displayID);
+
+    window = SDL_CreateWindow("Eopp Engine", mode->w, mode->h, SDL_WINDOW_VULKAN);
 
     if (!window) {
         std::cout << "Error creating window" << SDL_GetError() << std::endl;
